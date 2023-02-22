cc0787c0b1e8ce19796fae0d715de4ac11edb614
poysa213 <y.hanaia@esi-sba.dz>
2023-02-22 18:28:59 +0100

    delete some #

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -3,10 +3,10 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth import authenticate
 
 
-from authentication.models import User
+# from authentication.models import User
 from rest_framework import serializers
 
-# User = get_user_model()
+User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -21,43 +21,6 @@ class LoginUserSerializer(serializers.Serializer):
         trim_whitespace=False
     )
 
-    # def validate(self, attrs):
-    #     email = attrs.get('email')
-    #     username = attrs.get('username')
-    #     password = attrs.get('password')
-        
-
-    #     # if username and password:
-    #     #     user = authenticate(
-    #     #         request=self.context.get('request'),
-    #     #         username=username,
-    #     #         password=password
-    #     #     )
-
-    #     #     if not user:
-    #     #         msg = ('Unable to authenticate with provided credentials',)
-    #     #         raise serializers.ValidationError(msg=msg,code='authetication')
-    #     #     else:
-    #     #      attrs['user'] = user
-    #     if email and password:
-    #         user = authenticate(
-    #             request=self.context.get('request'),
-    #             email=email,
-    #             password=password
-    #         )
-
-    #         if not user:
-    #             msg = ('Unable to authenticate with provided credentialsdfs',)
-    #             raise serializers.ValidationError(msg=msg,code='authetication')
-    #         else:
-    #          attrs['user'] = user
-
-    #     else:
-    #             msg = ('Must include "username" and "password".')
-    #             raise serializers.ValidationError(msg, code='authorization')
-        
-    #     return
-    
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
@@ -65,14 +28,6 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ('id', 'username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
-
-    # def validate(self, attrs):
-    #     email = attrs.get('email', '')
-    #     if email:
-    #         if User.objects.filter(email=email).exists():
-    #             raise serializers.ValidationError(
-    #                 {'email': ('Email is already in use')})
-    #     return super().validate(attrs)
 
     def create(self, validated_data):
         password_data = validated_data.pop('password')
@@ -85,7 +40,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 class RegisterAdminSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
+        fields = ('id', 'username', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
